df4d7446ec6cf54ecd979c778f475f4b3ec26b60
RTEastman <42660520+RTEastman@users.noreply.github.com>
2020-12-10 22:07:31 -0500

    Update 0-setup.sh

--- 0-setup.sh
+++ 0-setup.sh
@@ -46,17 +46,18 @@ echo "       Setup Language to US and set locale       "
 echo "-------------------------------------------------"
 sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
 locale-gen
-timedatectl --no-ask-password set-timezone America/Chicago
+timedatectl --no-ask-password set-timezone America/New_York
 timedatectl --no-ask-password set-ntp 1
 
 localectl --no-ask-password set-locale LANG="en_US.UTF-8" LC_COLLATE="" LC_TIME="en_US.UTF-8"
 
 # Set keymaps
-echo "this far"
+
 localectl --no-ask-password set-keymap us
 
 # Hostname
 hostnamectl --no-ask-password set-hostname $hostname
+echo "this far"
 
 # Add sudo no password rights
 sed -i 's/^# %wheel ALL=(ALL) NOPASSWD: ALL/%wheel ALL=(ALL) NOPASSWD: ALL/' /etc/sudoers
