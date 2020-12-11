4f8f340fac49058e721f8d25306eb9da4b81850b
RTEastman <42660520+RTEastman@users.noreply.github.com>
2020-12-10 21:25:23 -0500

    Update preinstall.sh

--- preinstall.sh
+++ preinstall.sh
@@ -71,7 +71,7 @@ echo "-- Bootloader Systemd Installation  --"
 echo "--------------------------------------"
 bootctl install
 cat <<EOF > /boot/loader/entries/arch.conf
-title Arch Linux  
+title East Linux  
 linux /vmlinuz-linux  
 initrd  /initramfs-linux.img  
 options root=${DISK}1 rw
