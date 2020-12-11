f05bcfc95109e77f7ff1473912dfbc7f7f4f5451
RTEastman <42660520+RTEastman@users.noreply.github.com>
2020-12-10 21:57:56 -0500

    Update 0-setup.sh

--- 0-setup.sh
+++ 0-setup.sh
@@ -44,7 +44,7 @@ sudo sed -i 's/COMPRESSXZ=(xz -c -z -)/COMPRESSXZ=(xz -c -T $nc -z -)/g' /etc/ma
 echo "-------------------------------------------------"
 echo "       Setup Language to US and set locale       "
 echo "-------------------------------------------------"
-sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
+sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
 locale-gen
 timedatectl --no-ask-password set-timezone America/Chicago
 timedatectl --no-ask-password set-ntp 1
